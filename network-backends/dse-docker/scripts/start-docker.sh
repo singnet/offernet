f4c84625c350b33be2899c332c72e89d10861cb7
Viktoras Veitas <vveitas@gmail.com>
2018-03-28 13:18:20 +0200

    script for starting all dse related docker containers with remote ports on local server

--- network-backends/dse-docker/scripts/start-docker.sh
+++ network-backends/dse-docker/scripts/start-docker.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 # deleting all containers (not very efficient, but needed in order to run a container with the same name)
+docker stop $(docker ps -a -q)
 docker ps -q -a | xargs docker rm
 
 # start dse-server with graph enabled
-docker run -e DS_LICENSE=accept --name my-dse -d store/datastax/dse-server:5.1.6 -g
+docker run -e DS_LICENSE=accept -e LISTEN_ADDRESS=127.0.0.1 -e START_RPC=true --name dse -d -p 8182:8182 -p 9042:9042 store/datastax/dse-server:5.1.6 -g
+
+# start dse-studio
+docker run -e DS_LICENSE=accept --name dse-studio -d -p 9091:9091 --link dse datastax/dse-studio
+
+# start dse-opscenter
+docker run -e DS_LICENSE=accept --name opscenter -d -p 8888:8888 datastax/dse-opscenter
