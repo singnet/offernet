0424649bea8a43ec3462d367a61fbbf0996e37f0
kabir <kabir.33y@gmail.com>
2018-04-05 19:56:49 +0200

    commented out running opscenter doecker image by defalult

--- network-backends/dse-docker/scripts/start-docker.sh
+++ network-backends/dse-docker/scripts/start-docker.sh
@@ -7,8 +7,8 @@ docker ps -q -a | xargs docker rm
 # start dse-server with graph enabled
 docker run -e DS_LICENSE=accept -e LISTEN_ADDRESS=127.0.0.1 -e START_RPC=true --name dse -d -p 8182:8182 -p 9042:9042 store/datastax/dse-server:5.1.6 -g
 
-# start dse-studio
+# start dse-studio (not needed for tests)
 docker run -e DS_LICENSE=accept --name dse-studio -d -p 9091:9091 --link dse datastax/dse-studio
 
 # start dse-opscenter
-docker run -e DS_LICENSE=accept --name opscenter -d -p 8888:8888 datastax/dse-opscenter
+# docker run -e DS_LICENSE=accept --name opscenter -d -p 8888:8888 datastax/dse-opscenter
