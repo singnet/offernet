6404a8f618676e5f40f52ab31c2bee32b5950068
Viktoras Veitas <vveitas@gmail.com>
2018-07-15 22:49:08 +0200

    starting prometheus docker in detached mode

--- analysis/scripts/start_prometheus.sh
+++ analysis/scripts/start_prometheus.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # launch Prometheus in docker container 
-docker run -p 9090:9090 -v $PWD/analysis/configs/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
+docker run -p 9090:9090 -d -v $PWD/analysis/configs/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
