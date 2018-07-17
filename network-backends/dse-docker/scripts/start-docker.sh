25e52c956819fe38ec0e9eae62385dcf72da4538
Viktoras Veitas <vveitas@gmail.com>
2018-07-18 01:35:00 +0200

    dse-server docker does not start correctly with --memory option.../

--- network-backends/dse-docker/scripts/start-docker.sh
+++ network-backends/dse-docker/scripts/start-docker.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # deleting all containers (not very efficient, but needed in order to run a container with the same name)
-docker stop $(docker ps -a -q)
+docker stop $(docker ps --filter="name=dse" -a -q)
 docker ps -q -a | xargs docker rm
 
 # start dse-opscenter
-docker run -e DS_LICENSE=accept --name opscenter -d -p 8888:8888 --net=host datastax/dse-opscenter
+docker run -e DS_LICENSE=accept --name dse-opscenter -d -p 8888:8888 --net=host datastax/dse-opscenter
 
 # start dse-server with graph enabled
 docker run -e DS_LICENSE=accept -e LISTEN_ADDRESS=127.0.0.1 -e START_RPC=true --net=host --name dse -d -p 8182:8182 -p 9042:9042 -p 61621:61621 datastax/dse-server -g
