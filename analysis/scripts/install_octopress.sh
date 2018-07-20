c0fac3660405d0fc23bfb431ff5abc6ec7dafe79
Viktoras Veitas <vveitas@gmail.com>
2018-07-21 00:59:15 +0200

    deleted files not needed anymore

--- analysis/scripts/install_octopress.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd $PWD/analysis/elabnotebook-generate/
-if  [ ! -d octopress-2.0 ]; then
-	wget https://github.com/imathis/octopress/archive/v2.0.tar.gz;
-	tar -zxf v2.0.tar.gz;
-	rm v2.0.tar.gz;
-fi;
