fbf8fa05e15076fb01ea0d115fe4878d271fde54
kabirkbr <kabir@singularitynet.io>
2018-07-27 02:29:07 +0200

    added a port forwarding for offernet visualization server

--- scripts/port_forwarding_to_on-hetzner.sh
+++ scripts/port_forwarding_to_on-hetzner.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+This does not work if run from bash script -- need to copy and run from console (for now)
 
 ssh \
 -L 5601:127.0.0.1:5601 \
@@ -6,6 +6,6 @@ ssh \
 -L 8888:127.0.0.1:8888 \
 -L 8787:127.0.0.1:8787 \
 -L 8080:127.0.0.1:8080 \
--L 80:127.0.0.1:80 \
 -L 4000:127.0.0.1:4000 \
+-L 3000:127.0.0.1:3000 \
 kabir@on-hetzner.host
