ccd7df7248f00daff2d587454a4d8f02466a1123
kabir <vveitas@gmail.com>
2018-08-26 13:25:30 +0200

    separate bookdown for discussion documents; build script modified accordingly

--- scripts/build-elabnotebook-bookdown.sh
+++ scripts/build-elabnotebook-bookdown.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cd docs/decentralized-vs-centralized-bookdown
+#cd docs/decentralized-vs-centralized-bookdown #for experiment No1
+cd docs/discussion # for discussion and interpretation of experiments
 Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook")'
