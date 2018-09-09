c1ebe5b53d04ea9f2f8a109dcf47b3a52a9ece77
kabirkbr <kabir@singularitynet.io>
2018-09-09 23:23:11 +0200

    streamlined build notebook script for all books

--- scripts/build-elabnotebook-bookdown.sh
+++ scripts/build-elabnotebook-bookdown.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# 1: change directory to the desired bookdown source:
+
+#cd docs/offernet-docs-bookdown
 #cd docs/decentralized-vs-centralized-bookdown #for experiment No1
+#cd docs/additional-centralized-vs-decentralized # for discussion and interpretation of experiments
+#cd docs/more-centralized-vs-decentralized # for discussion and interpretation of experiments
 cd docs/discussion # for discussion and interpretation of experiments
+#cd docs/original-decentralized-vs-centralized-analysis
+
+
+# 2: run the script
 Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook")'
